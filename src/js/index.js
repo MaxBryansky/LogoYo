397c05a3ec28e999fe988eab6daa8bf4e74f7637
MaxBryansky <bryanskymaks1992@gmail.com>
2023-10-24 06:11:05 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
+
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  speed: 1000,
+  slidesPerView: 2,
+  spaceBetween: 40,
+  keyboard: {
+    enabled: true
+  },
+
+  navigation: {
+    prevEl: '#swiper-prev',
+    nextEl: '#swiper-next',
+  },
+});
